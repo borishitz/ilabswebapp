92b5805a6b93e466af0d8f752d065375677dea36
borishitz <borishitz@gmail.com>
2024-02-27 09:23:51 +0100

    edited contact form for information

--- src/main/java/com/noah/sarknext2/noah/sarknext2/entity/Contact.java
+++ src/main/java/com/noah/sarknext2/noah/sarknext2/entity/Contact.java
@@ -1,48 +1,30 @@
 package com.noah.sarknext2.noah.sarknext2.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contact {
     @Id
+
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
+
+    private String firstName;
+
+    private String lastName;
 
     private String email;
 
     private Long phoneNumber;
-    private String fullNames;
 
-    public Long getId() {
-        return id;
-    }
+    private String messageDetail;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getFullNames() {
-        return fullNames;
-    }
-
-    public void setFullNames(String fullNames) {
-        this.fullNames = fullNames;
-    }
 }
